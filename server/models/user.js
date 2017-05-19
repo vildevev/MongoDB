5e2ad1b77feb6a5954cf77223530436580028dcc
“Vilde <vilde@vevatne.no>
2017-05-19 11:30:32 -0700

    Installs body-parser and express

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+
 var User = mongoose.model('User', {
   name: {
     type: String,
--- server/server.js
+++ server/server.js
@@ -1,15 +1,3 @@
 var {mongoose} = require('./db/mongoose.js');
-var Todo = require('./db/models/todo.js');
-var User = require('./db/models/user.js');
-
-var newTodo = new Todo({
-  text: "Cook dinner"
-});
-
-newTodo.save().then((docs) => {
-  console.log('Save Todo', docs)
-}, (e) => {
-  console.log("Unable to save")
-});
-
-mongoose.disconnect();
+var {Todo} = require('./db/models/todo.js');
+var {User} = require('./db/models/user.js');
