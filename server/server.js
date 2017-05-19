8784b5f78b94f1f6fb7c0231391cc98c8e26be9a
“Vilde <vilde@vevatne.no>
2017-05-19 11:42:40 -0700

    Adds app.listen for port

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./db/models/todo.js');
-var {User} = require('./db/models/user.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("You have successfully connected on port " + port)
+});
