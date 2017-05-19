32725639cef59e417bf478d2c736bbcddd81983d
“Vilde <vilde@vevatne.no>
2017-05-19 12:06:09 -0700

    Adds bodyParser to post request

--- server/server.js
+++ server/server.js
@@ -6,6 +6,21 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  console.log(req.body);
+  var newTodo = new Todo({
+    title: req.body.title,
+    completed: req.body.completed
+  })
+  newTodo.save().then((doc) => {
+    console.log("it was saved", doc)
+  }, (e) => {
+    console.log("unable to save", e)
+  })
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
